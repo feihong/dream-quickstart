9cbe27accb73eb11acaff288681c71b95038be8f
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-22 22:18:00 -0600

    Add promises to count request middleware

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Answer yes when it asks you `Do you want opam to modify ~/.zshrc?`.
     asdf global opam latest
     opam init # might take a while
     eval $(opam env)
-    opam install dune merlin ocaml-lsp-server odoc ocamlformat utop dune-release
+    opam install dune merlin ocaml-lsp-server ocamlformat utop dune-release
 
 To see what switches are available, run `opam switch list-available`.
 
@@ -17,3 +17,7 @@ To see what switches are available, run `opam switch list-available`.
 
 - [Get Up and Running With OCaml](https://ocaml.org/docs/up-and-running)
 - [Dream Tutorial](https://github.com/aantron/dream/tree/master/example#readme)
+
+## Notes
+
+Dream doesn't yet work with OCaml 5.
--- tutorial/main.ml
+++ tutorial/main.ml
@@ -1,10 +1,17 @@
-let count = ref 0
 let () = Random.self_init ()
 
 module Middleware = struct
+  let successful = ref 0
+  let failed = ref 0
+
   let count_requests inner_handler request =
-    count := !count + 1;
-    inner_handler request
+    try%lwt
+      let%lwt response = inner_handler request in
+      successful := !successful + 1;
+      Lwt.return response
+    with exn ->
+      failed := !failed + 1;
+      raise exn
 end
 
 let () =
@@ -12,7 +19,10 @@ let () =
   @@ Dream.router
        [
          Dream.get "/" (fun _ ->
-             Dream.html (Printf.sprintf "Saw %i requests" !count));
+             Dream.html
+               (Printf.sprintf
+                  "Saw %3i successful requests, %3i failed requests"
+                  !Middleware.successful !Middleware.failed));
          Dream.get "/echo/:word" (fun request ->
              Dream.html (Dream.param request "word"));
          Dream.get "/hanzi" (fun _ ->
