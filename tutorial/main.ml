32625b4873f65483d03811a0c43018d208a8950f
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-22 21:54:07 -0600

    Add routes

--- tutorial/main.ml
+++ tutorial/main.ml
@@ -1,6 +1,12 @@
 let () =
   Random.self_init ();
-  
+
   Dream.run @@ Dream.logger
-  @@ fun _ ->
-  Dream.html ("早上好，世界！<br><h1>" ^ Hanzi.get_random_hanzi () ^ "</h1>")
+  @@ Dream.router
+       [
+         Dream.get "/" (fun _ -> Dream.html "Good morning, world");
+         Dream.get "/echo/:word" (fun request ->
+             Dream.html (Dream.param request "word"));
+         Dream.get "/hanzi" (fun _ ->
+             Dream.html ("早上好，世界！<br><h1>" ^ Hanzi.get_random_hanzi () ^ "</h1>"));
+       ]
